d6a54246da47af5d95f1e3c6691f20399e8258b6
unknown <dwiprajdey33720@gmail.com>
2026-05-31 12:53:41 +0530

    adding auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,2 +1,62 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const bcrypt = require("bcrypt.js")
+const generateToken = (id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:"7D"})
+}
+
+exports.registerUser = async(req,res)=>{
+    try {
+        const {name,email,password}=req.body;
+        const userExists = await User.findOne({email})
+        if(userExists){
+            return res.status(400).json({msg:"User already exists"})
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const user = await User.create({
+            name,email,password:hashedPassword,
+        })
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            token:generateToken(user._id)
+        })
+    } catch (error) {
+          res.status(500).json({msg:error.message})
+    }
+}
 
 
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (
+      user &&
+      (await bcrypt.compare(
+        password,
+        user.password
+      ))
+    ) {
+      return res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    }
+
+    res.status(401).json({
+      message: "Invalid credentials",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
