bfd84d2020511f98cf97990d76e13b4d70fd5c0a
unknown <dwiprajdey33720@gmail.com>
2026-05-31 12:33:08 +0530

    all curd operations in post controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,3 +14,43 @@ exports.createPost = async(req,res)=>{
         res.status(500).json({msg:error.message})
     }
 }
+
+exports.getPost = async(req,res)=>{
+    try {
+        const post = await Post.find().populate("author","name email");
+
+        res.json(post)
+
+    } catch (error) {
+          res.status(500).json({msg:error.message})
+    }
+}
+
+exports.updatePost = async(req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post){
+            return res.status(404).json({msg:"post not found"})
+        }
+        post.title = req.body.title || post.title;
+        post.content = req.body.content || post.content;
+        await post.save();
+        res.json(post)
+    } catch (error) {
+          res.status(500).json({msg:error.message})
+    }
+}
+
+exports.deletePost = async(req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id)
+        if(!post){
+            return res.status(404).json({msg:"Post not found"})
+
+        }
+        await post.deleteOne()
+        res.json({msg:"Post deleted"})
+    } catch (error) {
+          res.status(500).json({msg:error.message})
+    }
+}
